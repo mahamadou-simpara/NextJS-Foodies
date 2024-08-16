8727215af8a9d19e6373d750555b2e27db1b52a8
Mahamadou Simpara <mahamadousimpara@Simparas-MacBook-Air.local>
2024-08-16 12:14:04 +0000

    NextJS basics

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,20 @@
-function Meals () {
-    return <main>
-        <h1>Home Page Meals</h1>
-    </main>
+import Link from 'next/link';
+import classes from './page.module.css'
+import MealsGrid from '@/components/meals/meal-grid/meal-grid';
+
+
+function MealsPage () {
+    return <>
+        <header className={classes.header}>
+            <h1>Delicious meals, creeated 
+                <span className={classes.highlight}>by you</span>
+            </h1>
+            <p>Choose your favorite recipe and cook it yoursefl, It is easy and fun!</p>
+            <p className={classes.cta}><Link href="/meals/share">Share your favorite recipe</Link></p>
+        </header>
+        <main className={classes.main}><MealsGrid meals={[]} /></main>
+    </>
 }
 
 
-export default Meals;
+export default MealsPage;
