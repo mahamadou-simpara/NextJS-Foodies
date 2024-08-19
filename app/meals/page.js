6c61e5a05b9dc4d0f508bcc614d2a01825cd2e8a
Mahamadou Simpara <mahamadousimpara@Simparas-MacBook-Air.local>
2024-08-19 10:29:08 +0000

    How to load the page partially, loading the static part first and then waiting for the fetching part

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,25 +1,36 @@
 import Link from 'next/link';
-import classes from './page.module.css'
+import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meal-grid/meal-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
 
-async function MealsPage () {
-
-    const meals = await getMeals();
-
-
-    return <>
-        <header className={classes.header}>
-            <h1>Delicious meals, creeated 
-                <span className={classes.highlight}>by you</span>
-            </h1>
-            <p>Choose your favorite recipe and cook it yoursefl, It is easy and fun!</p>
-            <p className={classes.cta}><Link href="/meals/share">Share your favorite recipe</Link></p>
-        </header>
-        <main className={classes.main}><MealsGrid meals={meals} /></main>
+async function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, creeated
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yoursefl, It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals....</p>}>
+          <Meals />
+        </Suspense>
+      </main>
     </>
-}
-
+  );
+};
 
 export default MealsPage;
