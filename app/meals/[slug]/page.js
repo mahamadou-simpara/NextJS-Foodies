8031c29daaee16120ded31ae7cf49694db3d82a6
Mahamadou Simpara <mahamadousimpara@Simparas-MacBook-Air.local>
2024-08-19 14:49:45 +0000

    Adding Detail Page for fetching one single Meal

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,39 @@
-function MealIndex ({params}) {
-    return <main>
-        <h1>Meal Index Page</h1>
-        <h1>{params.slug}</h1>
-    </main>
-}
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
 
+function MealIndex({ params }) {
+  const meal = getMeal(params.slug);
+
+  if(!meal){
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            By <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
+}
 
 export default MealIndex;
